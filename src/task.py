b096645da3fc04c080e37e9f361a937ff4bda54b
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-04-29 11:37:00 -0400

    feat: update task interface

--- src/task.py
+++ src/task.py
@@ -1,29 +1,12 @@
 import os
-from task_interfaces import TaskInterface, SubscriptionLevels, TaskTypes
+from task_interfaces import StaticAnalysisTask, SubscriptionLevels
 
 
-class Task(TaskInterface):
+class Task(StaticAnalysisTask):
     """
     A simple and comprehensive vulnerability/misconfiguration scanner for containers and other artifacts.
     """
 
     name = "Trivy"
-    slug = "trivy"
-    pass_summary = ""
-    pass_text = ""
-    fail_summary = ""
-    fail_text = ""
     subscription_level = SubscriptionLevels.ENTERPRISE
-    actions = None
-
-    # For code format tasks, specify if they can automatically fix errors.
-    # If they can there will be a button for users to click in github ui.
-    can_fix = False
-
-    type = TaskTypes.CODE_FORMAT
     source_script_path = "%s/task.sh" % os.path.dirname(__file__)
-    handler = "task"
-
-    # Use the execute function if the task type is WORKFLOW
-    def execute(self, github_body, settings) -> bool:
-        pass
