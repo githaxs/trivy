7133d282763ceab47fcf83c7aec475cb4ad39451
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-08-01 14:12:22 -0400

    feat: use latest task capabilities

--- src/task.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-task() {
-    echo "Running Trivy"
-
-    trivy --quiet config --exit-code 1 .
-}
---
+++ task.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+run_task() {
+
+    if [ "$EVENT" == "push" ]; then
+        if [ -z "$AWS_REGION" ] || [ -z "$AWS_ACCOUNT_ID" ]; then
+            echo "AWS_REGION or AWS_ACCOUNT_ID are not set. Cannot report results to AWS Security Hub."
+        else
+            echo "Running Trivy"
+            trivy config --format template --template "@/var/task/src/report.tpl" -o report.asff .
+
+            # This is a hacky workaround for a known issue with Trivy.
+            # https://github.com/aquasecurity/trivy/issues/2023#issuecomment-1157747952
+            jq '.Findings' > report.json
+            aws securityhub batch-import-findings --findings file://report.json
+        fi
+
+    else
+        trivy --quiet config --exit-code 1 .
+    fi
+}
